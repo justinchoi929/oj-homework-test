5091635e50e56b622d476c89af948ad56177defc
cuixian.cx <cuixian.cx@alibaba-inc.com>
2026-04-09 16:09:33 +0800

    test: 故意写错的solution，隐藏用例会失败

--- chapter1/problem1/solution.py
+++ chapter1/problem1/solution.py
@@ -1,8 +1,10 @@
+# 暴力解法：双循环，但不处理负数情况（隐藏用例5会失败）
 nums = list(map(int, input().split()))
 target = int(input())
-seen = {}
-for i, n in enumerate(nums):
-    if target - n in seen:
-        print(seen[target - n], i)
-        break
-    seen[n] = i
+for i in range(len(nums)):
+    if nums[i] < 0:
+        continue
+    for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+            print(i, j)
+            exit()
