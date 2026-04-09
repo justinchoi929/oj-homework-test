03d499984cc69195b61860114bd31dbd3abb8cd9
cuixian.cx <cuixian.cx@alibaba-inc.com>
2026-04-09 15:46:34 +0800

    feat: 两数之和正确解法

--- chapter1/problem1/solution.py
+++ chapter1/problem1/solution.py
@@ -1,2 +1,8 @@
-a, b = map(int, input().split())
-print(a + b)
+nums = list(map(int, input().split()))
+target = int(input())
+seen = {}
+for i, n in enumerate(nums):
+    if target - n in seen:
+        print(seen[target - n], i)
+        break
+    seen[n] = i
